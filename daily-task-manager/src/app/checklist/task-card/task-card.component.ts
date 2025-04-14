e7559a1cc08515c8dbfdd1b3e24015485435c424
rc-devs <rc.devs9@gmail.com>
2025-04-14 11:22:13 -0500

    move dailyTasks to service

--- daily-task-manager/src/app/checklist/task-card/task-card.component.ts
+++ daily-task-manager/src/app/checklist/task-card/task-card.component.ts
@@ -9,19 +9,6 @@ import { FormsModule } from '@angular/forms';
 })
 export class TaskCardComponent {
 
-  dailyTasks = signal([
-    { id:"t1", complete: false, task: "Make Bed"},
-    { id:"t2", complete: false, task: "Dishwasher"},
-    { id:"t3", complete: false, task: "Dogs Out"},
-    { id:"t4", complete: false, task: "Water Dogs/Cats"},
-    { id:"t5", complete: false, task: "Feed Dogs"},
-    { id:"t6", complete: false, task: "Feed Cats"},
-    { id:"t7", complete: false, task: "Litter"},
-    { id:"t8", complete: false, task: "Empty Trash"},
-    { id:"t9", complete: false, task: "Sweep"},
-    { id:"t10", complete: false, task: "Morning Meds"},
-    { id:"t11", complete: false, task: "Evening Meds"},
-    { id:"t12", complete: false, task: "Brush Down Animals"}]);
 
     toggleComplete(check: boolean, task: object){
      //const foundTask = this.dailyTasks.filter(( id => task === this.dailyTasks ))
