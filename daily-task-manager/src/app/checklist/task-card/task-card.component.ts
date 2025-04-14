71f101e2ce3713931995b8a9b1db85ff098a5921
rc-devs <rc.devs9@gmail.com>
2025-04-14 11:24:31 -0500

    get default dailyTasks from service

--- daily-task-manager/src/app/checklist/task-card/task-card.component.ts
+++ daily-task-manager/src/app/checklist/task-card/task-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ChecklistService } from '../../shared/services/checklist.service';
 
 @Component({
   selector: 'app-task-card',
@@ -9,6 +10,11 @@ import { FormsModule } from '@angular/forms';
 })
 export class TaskCardComponent {
 
+    constructor(private checklistService: ChecklistService){}
+
+    getDefaultChecklist(){
+      return this.checklistService.dailyTasks;
+    }
 
     toggleComplete(check: boolean, task: object){
      //const foundTask = this.dailyTasks.filter(( id => task === this.dailyTasks ))
