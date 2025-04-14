a6132690221525ecc04b58701fba0efd228cec11
rc-devs <rc.devs9@gmail.com>
2025-04-14 11:46:38 -0500

    create add-task component and create conditional statement for display, creat form inside the add-task component

--- daily-task-manager/src/app/checklist/checklist.component.ts
+++ daily-task-manager/src/app/checklist/checklist.component.ts
@@ -1,22 +1,24 @@
 import { Component } from '@angular/core';
 import { TaskCardComponent } from './task-card/task-card.component';
 import { ChecklistService } from '../shared/services/checklist.service';
+import { AddTaskComponent } from './add-task/add-task.component';
 
 @Component({
   selector: 'app-checklist',
-  imports: [TaskCardComponent],
+  imports: [TaskCardComponent, AddTaskComponent],
   templateUrl: './checklist.component.html',
   styleUrl: './checklist.component.css'
 })
 export class ChecklistComponent {
   constructor(private checklistService: ChecklistService){}
+  addTask = false;
 
   onSubmit(){
     this.checklistService.onSubmitHandler()
   }
 
   onAddNewTask(){
-    this.checklistService.addNewTask()
+    this.addTask = true;
   }
 
 }
