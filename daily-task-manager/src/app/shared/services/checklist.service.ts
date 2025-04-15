986b7f3cf836ff6afc8ae300ff76a3205fc914da
rc-devs <rc.devs9@gmail.com>
2025-04-15 17:22:57 -0500

    data object modified, addNewTask updated

--- daily-task-manager/src/app/shared/services/checklist.service.ts
+++ daily-task-manager/src/app/shared/services/checklist.service.ts
@@ -7,18 +7,18 @@ export class ChecklistService {
   constructor() { }
 
   dailyTasks = [
-    { id:"t1", complete: false, task: "Make Bed"},
-    { id:"t2", complete: false, task: "Dishwasher"},
-    { id:"t3", complete: false, task: "Dogs Out"},
-    { id:"t4", complete: false, task: "Water Dogs/Cats"},
-    { id:"t5", complete: false, task: "Feed Dogs"},
-    { id:"t6", complete: false, task: "Feed Cats"},
-    { id:"t7", complete: false, task: "Litter"},
-    { id:"t8", complete: false, task: "Empty Trash"},
-    { id:"t9", complete: false, task: "Sweep"},
-    { id:"t10", complete: false, task: "Morning Meds"},
-    { id:"t11", complete: false, task: "Evening Meds"},
-    { id:"t12", complete: false, task: "Brush Down Animals"}];
+    { id:"t1", status:'incomplete', task: "Make Bed"},
+    { id:"t2", status:'incomplete', task: "Dishwasher"},
+    { id:"t3", status:'incomplete', task: "Dogs Out"},
+    { id:"t4", status:'incomplete', task: "Water Dogs/Cats"},
+    { id:"t5", status:'incomplete', task: "Feed Dogs"},
+    { id:"t6", status:'incomplete', task: "Feed Cats"},
+    { id:"t7", status:'incomplete', task: "Litter"},
+    { id:"t8", status:'incomplete', task: "Empty Trash"},
+    { id:"t9", status:'incomplete', task: "Sweep"},
+    { id:"t10", status:'incomplete', task: "Morning Meds"},
+    { id:"t11", status:'incomplete', task: "Evening Meds"},
+    { id:"t12", status:'incomplete', task: "Brush Down Animals"}];
 
   public addTaskForm = false;
 
@@ -50,7 +50,7 @@ export class ChecklistService {
 
      this.dailyTasks.unshift({
        id: 't' + randomNumber,
-       complete: false,
+       status: 'incomplete',
        task: newTask,
      })
      //set form status to remove form once submitted
