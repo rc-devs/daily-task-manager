e03d76607a7db46f698b83263813dc9c9f622acd
rc-devs <rc.devs9@gmail.com>
2025-04-19 13:11:32 -0500

    clean up app ts and html

--- daily-task-manager/src/app/app.component.ts
+++ daily-task-manager/src/app/app.component.ts
@@ -1,21 +1,13 @@
-import { Component, signal } from '@angular/core';
-//import { ChecklistComponent } from './checklist/checklist.component';
-//import { HistoryComponent } from './history/history.component';
+import { Component } from '@angular/core';
 import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [/* ChecklistComponent, HistoryComponent */ MenuComponent],
+  imports: [ MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'daily-task-manager';
 
-  /* chooseDisplay = signal("")
-
-  handleChooseDisplay(choice: string){
-    this.chooseDisplay.set(choice);
-  }
- */
 }
