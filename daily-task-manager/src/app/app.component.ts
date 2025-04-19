4efa850c61fdaa0c6386d4d0838796d21b1325af
rc-devs <rc.devs9@gmail.com>
2025-04-19 13:10:39 -0500

    migrate choose menu to own component

--- daily-task-manager/src/app/app.component.ts
+++ daily-task-manager/src/app/app.component.ts
@@ -1,20 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { ChecklistComponent } from './checklist/checklist.component';
-import { HistoryComponent } from './history/history.component';
+//import { ChecklistComponent } from './checklist/checklist.component';
+//import { HistoryComponent } from './history/history.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ChecklistComponent, HistoryComponent],
+  imports: [/* ChecklistComponent, HistoryComponent */ MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'daily-task-manager';
 
-  chooseDisplay = signal("")
+  /* chooseDisplay = signal("")
 
   handleChooseDisplay(choice: string){
     this.chooseDisplay.set(choice);
   }
-
+ */
 }
